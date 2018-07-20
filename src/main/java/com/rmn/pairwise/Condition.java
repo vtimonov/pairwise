e212cd4a0ebc3c670633adb1ba4ef04678f2575b
Viacheslav Timonov <viacheslavtimonov@appnovation.com>
2018-07-20 16:32:25 +0300

    Override toString, equals, hashCode in Condition class

--- src/main/java/com/rmn/pairwise/Condition.java
+++ src/main/java/com/rmn/pairwise/Condition.java
@@ -50,4 +50,33 @@ public class Condition {
     public void setValueTwo(String valueTwo) {
         this.valueTwo = valueTwo;
     }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Condition)) return false;
+
+        Condition condition1 = (Condition) o;
+
+        if (condition != condition1.condition) return false;
+        if (nameOne != null ? !nameOne.equals(condition1.nameOne) : condition1.nameOne != null) return false;
+        if (valueOne != null ? !valueOne.equals(condition1.valueOne) : condition1.valueOne != null) return false;
+        if (nameTwo != null ? !nameTwo.equals(condition1.nameTwo) : condition1.nameTwo != null) return false;
+        return valueTwo != null ? valueTwo.equals(condition1.valueTwo) : condition1.valueTwo == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = condition;
+        result = 31 * result + (nameOne != null ? nameOne.hashCode() : 0);
+        result = 31 * result + (valueOne != null ? valueOne.hashCode() : 0);
+        result = 31 * result + (nameTwo != null ? nameTwo.hashCode() : 0);
+        result = 31 * result + (valueTwo != null ? valueTwo.hashCode() : 0);
+        return result;
+    }
 }
